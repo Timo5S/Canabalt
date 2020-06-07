117d918a86c57ace604afc2a25e2a0a8f8624c74
T.S.Omta <timo.omta@hotmail.com>
2020-06-07 12:51:06 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Canabalt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
